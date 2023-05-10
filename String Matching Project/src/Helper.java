ceb29e86661ec62ab0602e8c3a4f231257f2f136
Ilker Keklik <ilkerkeklik50@gmail.com>
2023-05-10 16:42:53 +0300

    Bad Symbol,Good Suffix Implemented successfully Tested-by: @ilkerkeklik01 Test required

--- String Matching Project/src/Helper.java
+++ String Matching Project/src/Helper.java
@@ -6,11 +6,64 @@ public class Helper {
 
 
     public static void main(String[] args) {
-        String pattern = "CANAB";
+        String pattern = "LAMALAMAL";
 
         Map<Character,Integer> map = getBadSymbolTable(pattern);
 
+        System.out.println("For pattern \""+pattern+"\"");
+        System.out.println("Bad Symbol Table");
         System.out.println(map);
+        System.out.println("Good Suffix Table");
+        System.out.println(getGoodSuffixTable(pattern));
+
+
+    }
+
+    //Good Suffix Table returns Map<k,shift size>
+    public static Map<Integer,Integer> getGoodSuffixTable(String pattern){
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+
+
+        //iterate # of character match
+        outerLoop:
+        for(int k=1;k<=pattern.length()-1;k++){
+            int preceedingIndex = pattern.length()-k-1;
+            char preceeding = pattern.charAt(preceedingIndex);
+            String match = pattern.substring(preceedingIndex+1);
+
+            //iterate preceeding part of the pattern
+            for(int i= preceedingIndex;i>=0;i--){
+                char preOfCheck= 59999;//if check part is at the beginning
+                if(i!=0){
+                    preOfCheck = pattern.charAt(i-1);
+                }
+                String check = pattern.substring(i,i+k);
+
+                if (check.equals(match)&&preOfCheck!=preceeding){
+                    map.put(k,preceedingIndex+1-i);
+                    continue outerLoop;
+                }
+
+            }
+            //partial match control
+            for(int j = preceedingIndex+2;j<=pattern.length()-1;j++){
+                String partialCheckStr = pattern.substring(j);
+                if(pattern.startsWith(partialCheckStr)){
+                    map.put(k,j);
+                    continue outerLoop;
+                }
+            }
+            //else
+            map.put(k,pattern.length());
+
+        }
+
+
+
+        return map;
+
     }
 
 
@@ -42,6 +95,8 @@ public class Helper {
 
         return map;
     }
+
+
 
 
 
