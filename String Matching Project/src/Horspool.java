95d17d9d8a4241dd033ebe3d1c20640566fc875b
yduman <yusufdumans61@gmail.com>
2023-05-11 16:51:16 +0300

    bug fix

--- String Matching Project/src/Horspool.java
+++ String Matching Project/src/Horspool.java
@@ -20,30 +20,30 @@ public class Horspool extends Algorithm{
             comparison++;
 
             while(j >= 0 && text.charAt(i + j) == pattern.charAt(j)){
-                if (j == 0){
+                if (j <= 0){
                     matchIndexes.add(i);
                     isFullMatch = true;
                 }
                 comparison++;
                 j--;
             }
-            i += getShiftSize(isFullMatch, text, pattern);
+            i += getShiftSize(isFullMatch, text, pattern, i);
         }
 
         return matchIndexes;
     }
 
-    int getShiftSize(boolean isFullMatch, String text, String pattern){
+    int getShiftSize(boolean isFullMatch, String text, String pattern, int i){
         int shiftSize = 0;
         Map<Character, Integer> badSymbolTable =  Helper.getBadSymbolTable(pattern);
-        int lengthOfText = text.length();
+
         int lengthOfPattern = pattern.length();
 
         if (isFullMatch){
             shiftSize = lengthOfPattern;
         }
         else{
-            char lastCharOfPattern = pattern.charAt(lengthOfPattern - 1);
+            char lastCharOfPattern = text.charAt(i + lengthOfPattern - 1);
             shiftSize = badSymbolTable.getOrDefault(lastCharOfPattern, lengthOfPattern); // if key doesn't exist in map, get lengthOfPattern
         }
         return shiftSize;
